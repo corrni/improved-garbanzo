50ed6e55372a090c99bf7cd51de4f388229fe151
Bruno <iam@corrni.io>
2018-09-23 15:49:50 +0200

    7.4.2 -- Accessing route params

--- src/app/forums/forum/forum.component.ts
+++ src/app/forums/forum/forum.component.ts
@@ -11,8 +11,19 @@ import { Forum } from '../services/data';
 export class ForumComponent implements OnInit {
   forum: Forum;
 
-  constructor(private forumsService: ForumsService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private forumsService: ForumsService
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.forum = this.forumsService.forum(params['forum_alias']);
+      console.log(this.forum);
+      if (!this.forum)
+        this.router.navigate(['/not-found']);
+    });
+  }
 
 }
