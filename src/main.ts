0bbc062bebc0438aacd72a9e739a43d6e995419f
Bruno <iam@corrni.io>
2018-09-23 16:28:43 +0200

    Enable HMR

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,21 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { hmrBootstrap } from './hmr';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+const bootstrap = () =>
+  platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (environment.hmr) {
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for Webpack');
+  }
+} else {
+  bootstrap();
+}
